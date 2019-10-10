7ffcd4d9ca3eea53e56448dd23b48b838d0b14a8
廖 <lsp@nongline.com>
2019-10-10 14:23:10 +0800

    参数分离

--- readme.md
+++ readme.md
@@ -1,5 +1,8 @@
+
 ```
+cp .env.eample .env
 git clone https://github.com/liaoshengping/dnmp.git
 docker-compose up
 curl localhost
 ```
+>在开发环境中，可能会有权限问题，.env 中配置MYSQL_DATAPATH 的储存位置 /home/mysql
--- www/demo/index.php
+++ www/demo/index.php
@@ -1,2 +1,19 @@
 <?php
-echo 'hello im demo';
+echo 'demo ';
+$redis = new Redis();
+
+$redis->connect('redis', 6379); //连接Redis
+
+$redis->set( "testKey" , "连接redis成功"); //设置测试key
+
+echo $redis->get('testKey').PHP_EOL;
+
+
+$dbms='mysql';     //数据库类型
+$host='mysql'; //数据库主机名
+$dbName='mysql';    //使用的数据库
+$user='root';      //数据库连接用户名
+$pass='root';          //对应的密码
+$dsn="$dbms:host=$host;dbname=$dbName";
+$dbh = new PDO($dsn, $user, $pass); //初始化一个PDO对象
+echo "数据库连接成功<br/>";
